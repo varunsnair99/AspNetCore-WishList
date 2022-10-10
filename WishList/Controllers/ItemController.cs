78af2ea90236675cb7cb667c4e38626424ae1f8c
Varun S Nair <varunsnair99@gmail.com>
2022-10-10 17:35:17 +0530

    CommitItem

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var listofItems = _context.Items.ToList();
+            return View("Index",listofItems);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
