d609080996c986c4dfe3dbfc5188bbf3bbd4b49e
Varun S Nair <varunsnair99@gmail.com>
2022-10-10 17:39:24 +0530

    CommitItem1

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -11,9 +11,9 @@ namespace WishList.Controllers
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public ItemController(ApplicationDbContext applicationDbContext)
+        public ItemController(ApplicationDbContext context)
         {
-            _context = applicationDbContext;
+            _context = context;
         }
         public IActionResult Index()
         {
